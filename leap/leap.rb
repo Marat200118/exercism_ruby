d73fc7bfe8f599ca83dfe37f1a409f3150b1c3fa
Marat200118 <marat2001@inbox.lv>
2018-05-22 13:36:26 +0300

    leap year task

---
+++ leap/leap.rb
@@ -0,0 +1,15 @@
+class Year
+	def self.leap?(year)
+	  (year % 4 == 0) && ((year % 100 != 0) || (year % 100 == 0 && year % 400 == 0))
+  end
+end
+
+
+module BookKeeping 
+  VERSION = 3
+end
+
+
+ # a = (year % 4 == 0 && year % 100 != 0)
+
+ # b = (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)    
